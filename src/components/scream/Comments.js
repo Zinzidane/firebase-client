e1e989f6eedc53dfade2971c2a95b56b715b7baa
imuliarchyk <imuliarchyk@orion.intra>
2019-07-08 13:19:58 +0300

    Refactor comments component with react hooks

--- src/components/scream/Comments.js
+++ src/components/scream/Comments.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
@@ -20,56 +20,54 @@ const styles = (theme) => ({
   }
 });
 
-class Comments extends Component {
-  render() {
-    const { comments, classes } = this.props;
-    return (
-      <Grid container>
-        {comments.map((comment, index) => {
-          const { body, createdAt, userImage, userHandle } = comment;
-          return (
-            <Fragment key={createdAt}>
-              <Grid item sm={12}>
-                <Grid container>
-                  <Grid item sm={2}>
-                    <img
-                      src={userImage}
-                      alt="comment"
-                      className={classes.commentImage}
-                    />
-                  </Grid>
-                  <Grid item sm={9}>
-                    <div className={classes.commentData}>
-                      <Typography
-                        variant="h5"
-                        component={Link}
-                        to={`/users/${userHandle}`}
-                        color="primary"
-                      >
-                        {userHandle}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary">
-                        {dayjs(createdAt).format('h:mm a, MMMM DD YYYY')}
-                      </Typography>
-                      <hr className={classes.invisibleSeparator} />
-                      <Typography variabnt="body1">{body}</Typography>
-                    </div>
-                  </Grid>
+const comments = props => {
+  const { comments, classes } = props;
+  return (
+    <Grid container>
+      {comments.map((comment, index) => {
+        const { body, createdAt, userImage, userHandle } = comment;
+        return (
+          <Fragment key={createdAt}>
+            <Grid item sm={12}>
+              <Grid container>
+                <Grid item sm={2}>
+                  <img
+                    src={userImage}
+                    alt="comment"
+                    className={classes.commentImage}
+                  />
+                </Grid>
+                <Grid item sm={9}>
+                  <div className={classes.commentData}>
+                    <Typography
+                      variant="h5"
+                      component={Link}
+                      to={`/users/${userHandle}`}
+                      color="primary"
+                    >
+                      {userHandle}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {dayjs(createdAt).format('h:mm a, MMMM DD YYYY')}
+                    </Typography>
+                    <hr className={classes.invisibleSeparator} />
+                    <Typography variabnt="body1">{body}</Typography>
+                  </div>
                 </Grid>
               </Grid>
-              {index !== comments.length - 1 && (
-                <hr className={classes.visibleSeparator} />
-              )}
-            </Fragment>
-          );
-        })}
-      </Grid>
-    );
-  }
+            </Grid>
+            {index !== comments.length - 1 && (
+              <hr className={classes.visibleSeparator} />
+            )}
+          </Fragment>
+        );
+      })}
+    </Grid>
+  );
 }
 
-Comments.propTypes = {
+comments.propTypes = {
   comments: PropTypes.array.isRequired
 };
 
-export default withStyles(styles)(Comments);
+export default withStyles(styles)(comments);
