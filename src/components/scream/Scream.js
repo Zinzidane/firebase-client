9bf9d9302225228906063cc397a8b47909b93314
imuliarchyk <imuliarchyk@orion.intra>
2019-07-09 11:57:31 +0300

    Refactor Scream component with react hooks

--- src/components/scream/Scream.js
+++ src/components/scream/Scream.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
@@ -33,66 +33,64 @@ const styles = {
   }
 };
 
-class Scream extends Component {
-  render() {
-    dayjs.extend(relativeTime);
-    const {
-      classes,
-      scream: {
-        body,
-        createdAt,
-        userImage,
-        userHandle,
-        screamId,
-        likeCount,
-        commentCount
-      },
-      user: {
-        authenticated,
-        credentials: { handle }
-      }
-    } = this.props;
+const Scream = props => {
+  dayjs.extend(relativeTime);
+  const {
+    classes,
+    scream: {
+      body,
+      createdAt,
+      userImage,
+      userHandle,
+      screamId,
+      likeCount,
+      commentCount
+    },
+    user: {
+      authenticated,
+      credentials: { handle }
+    }
+  } = props;
 
-    const deleteButton =
-      authenticated && userHandle === handle ? (
-        <DeleteScream screamId={screamId} />
-      ) : null;
-    return (
-      <Card className={classes.card}>
-        <CardMedia
-          image={userImage}
-          title="Profile image"
-          className={classes.image}
+  const deleteButton =
+    authenticated && userHandle === handle ? (
+      <DeleteScream screamId={screamId} />
+    ) : null;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        image={userImage}
+        title="Profile image"
+        className={classes.image}
+      />
+      <CardContent className={classes.content}>
+        <Typography
+          variant="h5"
+          component={Link}
+          to={`/users/${userHandle}`}
+          color="primary"
+        >
+          {userHandle}
+        </Typography>
+        {deleteButton}
+        <Typography variant="body2" color="textSecondary">
+          {dayjs(createdAt).fromNow()}
+        </Typography>
+        <Typography variant="body1">{body}</Typography>
+        <LikeButton screamId={screamId} />
+        <span>{likeCount} Likes</span>
+        <MyButton tip="comments">
+          <ChatIcon color="primary" />
+        </MyButton>
+        <span>{commentCount} comments</span>
+        <ScreamDialog
+          screamId={screamId}
+          userHandle={userHandle}
+          openDialog={props.openDialog}
         />
-        <CardContent className={classes.content}>
-          <Typography
-            variant="h5"
-            component={Link}
-            to={`/users/${userHandle}`}
-            color="primary"
-          >
-            {userHandle}
-          </Typography>
-          {deleteButton}
-          <Typography variant="body2" color="textSecondary">
-            {dayjs(createdAt).fromNow()}
-          </Typography>
-          <Typography variant="body1">{body}</Typography>
-          <LikeButton screamId={screamId} />
-          <span>{likeCount} Likes</span>
-          <MyButton tip="comments">
-            <ChatIcon color="primary" />
-          </MyButton>
-          <span>{commentCount} comments</span>
-          <ScreamDialog
-            screamId={screamId}
-            userHandle={userHandle}
-            openDialog={this.props.openDialog}
-          />
-        </CardContent>
-      </Card>
-    );
-  }
+      </CardContent>
+    </Card>
+  );
 }
 
 Scream.propTypes = {
